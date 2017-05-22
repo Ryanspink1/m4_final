5be194b7c9383e3c816a36f428aa6b38309dcfde
Ryan Spink <ryanspinktech@gmail.com>
2017-05-22 16:05:38 -0600

    feature test for filtering by read status

--- spec/features/user/user_can_filter_links_spec.rb
+++ spec/features/user/user_can_filter_links_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "user can edit a link url/title", :js => :true do
-  scenario "edit link read status" do
+RSpec.describe "user can filter links by url/title", :js => :true do
+  scenario "filter link by url/title" do
     user = User.create(id: 1, email:"user@user.com", password:"password")
     link = user.links.create(url:"www.facebook.com", title:"Facebook")
     link_two = user.links.create(url:"www.espn.com", title:"espn")
@@ -15,7 +15,7 @@ RSpec.describe "user can edit a link url/title", :js => :true do
 
     expect(page).to have_content(link.title)
     expect(page).to have_content(link.url)
-    
+
     expect(page).to_not have_content(link_two.title)
     expect(page).to_not have_content(link_two.url)
     expect(page).to_not have_content(link_three.title)
