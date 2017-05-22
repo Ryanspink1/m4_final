38f4a2a150d98b68781b6d639164082036f8d416
Ryan Spink <ryanspinktech@gmail.com>
2017-05-22 17:42:34 -0600

    show read and show unread actually working now

--- app/assets/javascripts/filter_by_read.js
+++ app/assets/javascripts/filter_by_read.js
@@ -1,11 +1,28 @@
 
 
 function filterByRead(boolean){
-  refreshLinkTable();
-  debugger;
+  clearTable()
+  var userID = $('#create-link-user-id').val()
+  $.ajax({
+    url: `http://localhost:3000/api/v1/linksreadstatus?id=${userID}&read=${boolean}`,
+    type: 'get'
+  }).done(appendLinkTable)
+}
+//     getLinkIndex()
+//     setTimeout(filterByReadData, 1000)
+//   // var linkTableData = $('.link-table-data')
+//   //   for(i=0;i<linkTableData.length;i++){
+//   //     if(linkTableData[i].children[2].innerHTML == boolean.toString()){}
+//   //     else{
+//   //       linkTableData[i].closest('tr').style.display = "none"
+//   //     }
+//   //   }
+// }
+
+
+function filterByReadData(boolean){
   var linkTableData = $('.link-table-data')
     for(i=0;i<linkTableData.length;i++){
-      debugger;
       if(linkTableData[i].children[2].innerHTML == boolean.toString()){}
       else{
         linkTableData[i].closest('tr').style.display = "none"
@@ -13,9 +30,9 @@ function filterByRead(boolean){
     }
 }
 
-function refreshLinkTable(){
-  var linkTableData = $('.link-table-data')
-    for(i=0;i<linkTableData.length;i++){
-        linkTableData[i].closest('tr').style.display = ""
-    }
-}
+// function refreshLinkTable(){
+//   var linkTableData = $('.link-table-data')
+//     for(i=0;i<linkTableData.length;i++){
+//         linkTableData[i].closest('tr').style.display = ""
+//     }
+// }
