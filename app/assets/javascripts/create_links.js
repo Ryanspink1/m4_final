7bf6645e94c6032ca343aa2276de90e137b3bd76
Ryan Spink <ryanspinktech@gmail.com>
2017-05-22 14:15:51 -0600

    mark as read/unread buttons for each table link entry work and update db via ajax put

--- app/assets/javascripts/create_links.js
+++ app/assets/javascripts/create_links.js
@@ -55,12 +55,13 @@ function appendLinkTable(data){
     if(data[i].read.toString() == "false"){
       $('#links-table').append(
         `<tr class="link-table-data">
-           <th class="table-url">${data[i].url}</th>
-           <th class="table-title">${data[i].title}</th>
-           <th class="table-read">${data[i].read}</th>
+           <th class="table-url">   ${data[i].url}</th>
+           <th class="table-title"> ${data[i].title}</th>
+           <th class="table-read">  ${data[i].read}</th>
            <th>
-           <button type="button" class="btn btn-default" id="mark-read">Mark Read</button>
+           <button type="button" class="btn btn-default" id="mark-read" onClick="editReadStatus(this, true)">Mark Read</button>
            </th>
+           <th style="display:none" class="table-link-id"> ${data[i].id}</th>
         `
       )
     }
@@ -71,8 +72,9 @@ function appendLinkTable(data){
           <th class="table-title">${data[i].title}</th>
           <th class="table-read">${data[i].read}</th>
           <th>
-          <button type="button" class="btn btn-default" id="mark-unread">Mark Unread</button>
+          <button type="button" class="btn btn-default" id="mark-unread" onClick="editReadStatus(this,false)">Mark Unread</button>
           </th>
+          <th style="display:none" class="table-link-id"> ${data[i].id}</th>
        `
      )
    }
@@ -87,4 +89,17 @@ function getLinkIndex(){
     url: `http://localhost:3000/api/v1/links?id=${userID}`,
     type: 'get'
   }).done(appendLinkTable)
+}
+
+function editReadStatus(page, boolean){
+  id = page.closest('tr').children[4].innerHTML
+  readStatus = boolean
+  var linkParams = {read:readStatus}
+debugger;
+  $.ajax({
+    url: `http://localhost:3000/api/v1/links/${id}`,
+    type: 'put',
+    data: linkParams,
+  }).done(setTimeout(getLinkIndex, 500))
+
 }
