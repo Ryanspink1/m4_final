4f6fa929f182994084661140e36a8a603990ea93
Ryan Spink <ryanspinktech@gmail.com>
2017-05-22 13:38:23 -0600

    beginning test for editing read status

--- app/assets/javascripts/create_links.js
+++ app/assets/javascripts/create_links.js
@@ -47,23 +47,35 @@ function clearTable(){
 
 function appendLinkTable(data){
   for(i=0;i<data.length;i++){
-  $('#links-table').append(
-    `<tr class="link-table-data">
-       <th class="table-url">${data[i].url}</th>
-       <th class="table-title">${data[i].title}</th>
-       <th class="table-read">${data[i].read}</th>
-       <th>
-       <button type="button" class="btn btn-default" id="mark-read">Mark Read</button>
-       <button type="button" class="btn btn-default" id="mark-unread">Mark Unread</button>
-       </th>
-    `
-  )
+    if(data[i].read.toString() == "false"){
+      $('#links-table').append(
+        `<tr class="link-table-data">
+           <th class="table-url">${data[i].url}</th>
+           <th class="table-title">${data[i].title}</th>
+           <th class="table-read">${data[i].read}</th>
+           <th>
+           <button type="button" class="btn btn-default" id="mark-read">Mark Read</button>
+           </th>
+        `
+      )
+    }
+   else{
+     $('#links-table').append(
+       `<tr class="link-table-data">
+          <th class="table-url">${data[i].url}</th>
+          <th class="table-title">${data[i].title}</th>
+          <th class="table-read">${data[i].read}</th>
+          <th>
+          <button type="button" class="btn btn-default" id="mark-unread">Mark Unread</button>
+          </th>
+       `
+     )
+   }
   }
 }
 
 function getLinkIndex(){
   clearTable()
-  debugger;
   var userID = $('#create-link-user-id').val()
   $.ajax({
     url: `http://localhost:3000/api/v1/links?id=${userID}`,
