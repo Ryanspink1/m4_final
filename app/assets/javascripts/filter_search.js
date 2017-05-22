cc472aaaeb4b53f4d65eff6132db3d982a5c0cb2
Ryan Spink <ryanspinktech@gmail.com>
2017-05-22 15:53:40 -0600

    filtering working but getting error in chrome console about getElementsByTagName of undefined

--- app/assets/javascripts/filter_search.js
+++ app/assets/javascripts/filter_search.js
@@ -9,9 +9,10 @@ function filterSearch(){
 
   for (i = 2; i < tr.length+2; i++) {
     td = tr[i].getElementsByTagName("th")[0];
+    tdOne = tr[i].getElementsByTagName("th")[1];
 
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+    if (td || tdOne) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || tdOne.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
