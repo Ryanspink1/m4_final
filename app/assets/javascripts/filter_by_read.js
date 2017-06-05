933325a297030e879fd43933ce6e6d54cf013046
Ryan Spink <ryanspinktech@gmail.com>
2017-06-05 11:20:36 -0600

    revert back for local use, working on heroku

--- app/assets/javascripts/filter_by_read.js
+++ app/assets/javascripts/filter_by_read.js
@@ -4,7 +4,7 @@ function filterByRead(boolean){
   clearTable()
   var userID = $('#create-link-user-id').val()
   $.ajax({
-    url: `https://urllockboxfinal.herokuapp.com/api/v1/linksreadstatus?id=${userID}&read=${boolean}`,
+    url: `http://localhost:3000/api/v1/linksreadstatus?id=${userID}&read=${boolean}`,
     type: 'get'
   }).done(appendLinkTable)
 }
