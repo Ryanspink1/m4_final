6b05d94790c3c06381e2f905711c996f6025b461
Ryan Spink <ryanspinktech@gmail.com>
2017-06-05 12:58:48 -0600

    adds more robust model testing for links and users

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Link, type: :model do
     it { should belong_to(:user) }
   end
 
+  context 'validations' do
+    it { should validate_presence_of(:url) }
+    it { should validate_presence_of(:title) }
+  end
+
+  it "should accept a valid url" do
+    link_one = Link.new(url: 'https://www.jabron.com/',title: 'jabron')
+
+    expect(link_one).to be_valid
+  end
+
+  it "should not accept an invalid url" do
+    link_two = Link.new(url: 'google',title: 'google')
+
+    expect(link_two).to_not be_valid
+  end
 end
