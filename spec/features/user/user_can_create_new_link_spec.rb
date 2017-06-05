b50baab420d12295b95d3de07d4d5257e143c150
Ryan Spink <ryanspinktech@gmail.com>
2017-06-05 15:51:40 -0600

    added sad path feature testing for creating and editing a link

--- spec/features/user/user_can_create_new_link_spec.rb
+++ spec/features/user/user_can_create_new_link_spec.rb
@@ -15,7 +15,6 @@ RSpec.describe "user can create new link", :js => :true do
 
 
     link = user.links
-    # binding.pry
 
 
     expect(page).to have_content('www.google.com')
@@ -24,4 +23,36 @@ RSpec.describe "user can create new link", :js => :true do
     expect(page).to have_content('Mark Read')
 
   end
+
+  scenario "link will not be created if title is invalid" do
+    user = User.create(id: 1, email:"user@user.com", password:"password")
+    user.links.create(url:"www.facebook.com", title:"facebook")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/"
+
+    fill_in 'Enter a URL', with: "www.google.com"
+    fill_in 'Enter a Title', with: ''
+    click_button("submit-new-link")
+
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to_not have_content("www.google.com")
+  end
+
+  scenario "link will not be created if url is invalid" do
+    user = User.create(id: 1, email:"user@user.com", password:"password")
+    user.links.create(url:"www.facebook.com", title:"facebook")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/"
+
+    fill_in 'Enter a URL', with: "google"
+    fill_in 'Enter a Title', with: 'google'
+    click_button("submit-new-link")
+
+    expect(page).to have_content("URL is an invalid URL")
+    expect(page).to_not have_content("google")
+  end
+
+
 end
--- spec/features/user/user_can_edit_link_spec.rb
+++ spec/features/user/user_can_edit_link_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "user can edit a link url/title", :js => :true do
   scenario "edit link read status" do
     user = User.create(id: 1, email:"user@user.com", password:"password")
     link = user.links.create(url:"www.facebook.com", title:"facebook")
-
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit '/links'
 
@@ -22,5 +22,80 @@ RSpec.describe "user can edit a link url/title", :js => :true do
 
     expect(page).to have_content('www.espn.com')
     expect(page).to have_content('ESPN')
+  end
+
+  scenario "cannot edit with invalid url" do
+    user = User.create(id: 1, email:"user@user.com", password:"password")
+    link = user.links.create(url:"www.facebook.com", title:"facebook")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/links'
+
+    expect(page).to have_content('Edit')
+
+    click_button 'Edit'
+
+    expect(current_path).to eq(edit_link_path(link))
+
+    fill_in 'url', with: ''
+    fill_in 'title', with: 'ESPN'
+    click_button 'submit'
+
+    expect(current_path).to eq(root_path)
+
+    expect(page).to_not have_content('www.espn.com')
+    expect(page).to_not have_content('ESPN')
+
+    expect(page).to have_content("URL can't be blank")
+  end
+
+  scenario "cannot edit with invalid url" do
+    user = User.create(id: 1, email:"user@user.com", password:"password")
+    link = user.links.create(url:"www.facebook.com", title:"facebook")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/links'
+
+    expect(page).to have_content('Edit')
+
+    click_button 'Edit'
+
+    expect(current_path).to eq(edit_link_path(link))
+
+    fill_in 'url', with: 'espn'
+    fill_in 'title', with: 'ESPN'
+    click_button 'submit'
+
+    expect(current_path).to eq(root_path)
+
+    expect(page).to_not have_content('www.espn.com')
+    expect(page).to_not have_content('ESPN')
+
+    expect(page).to_not have_content("URL is an invalid URL")
+  end
+
+  scenario "cannot edit with invalid title" do
+    user = User.create(id: 1, email:"user@user.com", password:"password")
+    link = user.links.create(url:"www.facebook.com", title:"facebook")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/links'
+
+    expect(page).to have_content('Edit')
+
+    click_button 'Edit'
+
+    expect(current_path).to eq(edit_link_path(link))
+
+    fill_in 'url', with: 'www.espn.com'
+    fill_in 'title', with: ''
+    click_button 'submit'
+
+    expect(current_path).to eq(root_path)
+
+    expect(page).to_not have_content('www.espn.com')
+    expect(page).to_not have_content('ESPN')
+
+    expect(page).to_not have_content("Title can't be blank")
   end
 end
